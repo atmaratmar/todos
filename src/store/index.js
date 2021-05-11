cb8ef831c30ab50b40ab176ed6fa73a7ba4cc398
atmaratmar <atmar@hotmail.dk>
2021-05-11 23:40:34 +0200

    REFATCORING  the  store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
-// Entrypoint for vuex
-
 import Vuex from 'vuex';
 import Vue from 'vue';
-import todos from './modules/Todos';
+import axios from 'axios';
+
 
 // Load vuex
 Vue.use(Vuex);
 
 // Create store
 export default new Vuex.Store({
-  modules: {
-    todos
-  }
+  state : {
+    todos: []
+  },
+  mutations : {
+      setTodos: (state, todos) => (state.todos = todos),
+      addTodo:(state,newTodo) => state.todos.unshift(newTodo)
+  },
+  getters : {
+    allTodos: (state) => {
+      return state.todos
+    }
+  },
+  actions : {
+      async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
+    
+        commit('setTodos', response.data);
+      },
+      async addTodo({commit},title){
+          const response = await axios.post(`https://jsonplaceholder.typicode.com/todos`,
+          {title:title,completed:false})
+    
+          commit('addTodo',response.data)
+      }
+    }
 });
