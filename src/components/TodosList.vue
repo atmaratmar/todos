1bd57d64cdaf00e1b535cf5fc97fde1f56f9ae9a
atmar kohistany <atmar@hotmail.dk>
2021-05-11 13:37:40 +0200

    FEAT todoslist filtertodo and addtodo component is added

---
+++ src/components/TodosList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>TodoList</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodosList"
+}
+</script>
+
+<style scoped>
+
+</style>
